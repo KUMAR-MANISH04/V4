42bd8babe78586cb1d1d93996560226210b27d34
MANEESH SINHA <56319939+KUMAR-MANISH04@users.noreply.github.com>
2023-02-05 18:33:05 +0530

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -72,7 +72,7 @@ const Hero = () => {
   const five = (
     <a
       className="email-link"
-       href={`mailto:${email}`}
+       href="7050maneesh@gmail.com"
       target="_blank"
       rel="noreferrer">
       Contact me!

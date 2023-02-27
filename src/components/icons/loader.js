7bc191a35c266f1ab74507c446a64ab2b7972ae3
MANEESH SINHA <56319939+KUMAR-MANISH04@users.noreply.github.com>
2023-02-28 04:26:10 +0530

    Update loader.js

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -18,8 +18,8 @@ const IconLoader = () => (
                   L 89, 28 z"
       />
     </g>
-    <text x="10" y="70" fill="currentColor" fontSize="45px" fontFamily="Consolas">
-      MK
+    <text x="30" y="70" fill="currentColor" fontSize="55px" fontFamily="Consolas">
+      M
     </text>
   </svg>
 );
